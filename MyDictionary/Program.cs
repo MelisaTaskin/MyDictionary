e459f02251c571ee7fbb64b5c95789bdda328fc6
Melisa <Melisa@DESKTOP-HH46EBS>
2021-02-22 01:12:00 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary2<string> City = new MyDictionary2<string>();
+
+            
+            MyDictionary2<int> Plate = new MyDictionary2<int>();
+            City.Add("Bayburt");
+            Plate.Add(69);
+            City.Add("Isparta");
+            Plate.Add(32);
+            Console.WriteLine(City.Count);
+            Console.WriteLine( Plate.Count );
+
+
+            
+        }
+    }
+}
